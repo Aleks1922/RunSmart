ad4f0710c7a36d919f24b6766bebf8913ea94fb7
Aleksey <kinderlolpingui@yandex.ru>
2022-04-18 14:03:38 +0300

    add section reviews and catalog

--- src/js/script.js
+++ src/js/script.js
@@ -10,4 +10,24 @@ $(document).ready(function () {
       }
     ]
   });
+
+  $('ul.tabs').on('click', 'li:not(.tabs__item--active)', function () {
+    $(this)
+      .addClass('tabs__item--active').siblings().removeClass('tabs__item--active')
+      .closest('div.container').find('div.catalog__wrapper').removeClass('catalog__wrapper--active').eq($(this).index()).addClass('catalog__wrapper--active');
+  });
+
+  $('.card__link').each(function (i) {
+    $(this).on('click', function (e) {
+      e.preventDefault();
+      $('.card__wrapper').eq(i).toggleClass('card__wrapper--active');
+    });
+  });
+
+  $('.card__back').each(function (i) {
+    $(this).on('click', function (e) {
+      e.preventDefault();
+      $('.card__wrapper').eq(i).toggleClass('card__wrapper--active');
+    });
+  });
 });
